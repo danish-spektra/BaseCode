1959e0c5b199e4877c74575e5d5ecaf2ff088d82
danish-spektra <danish.hajiameen@spektrasystems.com>
2026-06-19 17:35:56 +0530

    upd

---
+++ ARM/CloudLabs/dummy.ps1
@@ -0,0 +1,44 @@
+Param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AzureUserName,
+    [string]
+    $AzurePassword,
+    [string]
+    $AzureTenantID,
+    [string]
+    $AzureSubscriptionID,
+    [string]
+    $ODLID,
+    [string]
+    $DeploymentID,
+    [string]
+    $adminUsername,
+    [string]
+    $adminPassword,
+    [string]
+    $trainerUserName,
+    [string]
+    $trainerUserPassword
+)
+ 
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+
+#Import Common Functions
+$path = pwd
+$path=$path.Path
+$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+ 
+# Run Imported functions from cloudlabs-windows-functions.ps1
+WindowsServerCommon
+
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID 
+
+Enable-CloudLabsEmbeddedShadow $adminUsername $trainerUserName $trainerUserPassword
+
+Stop-Transcript
+ 
+Restart-Computer -Force
